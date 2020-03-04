01505f3504aa984b96deb0e81fcc3ab4ce5ef69b
andreystarenky <47681471+andreystarenky@users.noreply.github.com>
2020-03-04 02:08:15 -0500

    Added "Coming Soon!" labels for news and stats

--- app/src/main/java/com/starenkysoftware/coronavirus_tracker/ui/news/NewsFragment.java
+++ app/src/main/java/com/starenkysoftware/coronavirus_tracker/ui/news/NewsFragment.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.starenkysoftware.coronavirus_tracker.R;
@@ -22,14 +19,7 @@ public class NewsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         toolsViewModel =
                 ViewModelProviders.of(this).get(ToolsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_tools, container, false);
-        final TextView textView = root.findViewById(R.id.text_tools);
-        toolsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        View root = inflater.inflate(R.layout.fragment_news, container, false);
         return root;
     }
 }
